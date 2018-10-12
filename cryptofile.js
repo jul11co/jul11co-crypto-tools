bc7c5fb09058432fc9a2808d071e1ab9189489d1
Manh Thiep <manhthiep@gmail.com>
2018-10-12 17:59:06 +0700

    Update to 0.0.5 - crypto-index: support SQLite3 as index file format - crypto-index: make use of async APIs - crypto-folder: add TRASH folder to temporarily store removed files - crypto-mount: support remove files, remove folders (--read-write option needs to specify) - cryptofolder: add folder:list, folder:add, folder:remove to manage known folders - cryptofolder: add trash:list, trash:empty to manage trashed files - crypto-utils: use 'inquirer' instead of 'prompt' to get passphrase inputs from stdin - cryptor: update accordingly to crypto-index - crypto-browser: update accordingly to crypto-index

--- cryptofile.js
+++ cryptofile.js
@@ -116,7 +116,7 @@ var getEncryptionKey = function(opts, callback) {
     var ENC_KEY = cryptoUtils.generateEncryptionKey(options.passphrase, crypto_salt);
     return callback(null, ENC_KEY);
   } else {
-    cryptoUtils.getPromptPassphrase(opts, function(err, passphrase) {
+    cryptoUtils.getInputPassphrase(opts, function(err, passphrase) {
       if (err) {
         return callback(err);
       }
@@ -147,7 +147,7 @@ if (command == 'config') {
     console.log('Config saved.');
     process.exit();
   }else if (options.set_salt) {
-    cryptoUtils.getPromptSalt(function(err, salt) {
+    cryptoUtils.getInputSalt(function(err, salt) {
       if (err) {
         // console.log(err);
         console.log('');
